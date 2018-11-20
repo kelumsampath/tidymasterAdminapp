9781b5d85e9255eb99ce7fe20a875dc4ab924696
kelum <edirisingheemks@gmail.com>
2018-11-20 16:46:02 +0530

    logout completed

--- myapp/routes/app/Logout/index.js
+++ myapp/routes/app/Logout/index.js
@@ -1,25 +1,73 @@
 import React from "react";
 import { View, Text ,AsyncStorage, Alert } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+var config = require('./../../../screen/config')
 
 export default class Logout extends React.Component {
     constructor(props){
         super(props);
+        this.getToken();
         this.getDecision();
       }
       state={
         decision:'',
+        token:""
       };
+
+    handletoken=(text)=>{
+      this.setState({token:text})
+    }
+
     getDecision(){
         Alert.alert(
             'Logout comfirmation',
             'do you want to logout?',
             [
-            {text:'Yes', onPress:()=>this.removeToken()},
+            {text:'Yes', onPress:()=>this.requestlogout()},
             {text:'No', onPress:()=>Actions.pop()}
             ],
             { cancelable: false}
         )
+    }
+
+    async getToken(){
+        try{
+          let thistoken=await AsyncStorage.getItem("token");
+         // let token=JSON.stringify(thistoken)
+
+          //alert(a)
+          if(thistoken!=null){
+            this.handletoken(thistoken);
+          }else{
+            Actions.login();  
+          }
+        }catch(error){
+          alert(error);
+          Actions.login();
+        }
+      }
+
+    requestlogout(){
+        fetch(config.config.hostname+'/user/logout', {
+            method: 'GET',
+            headers: {
+              "Authorization": this.state.token,
+              'Content-Type': 'application/json'
+            }
+            
+        })
+      
+            .then((response) => response.json())
+            .then((res) => {
+      
+                if (res.state === true) {
+                  this.removeToken()
+                  
+                } else {
+                    alert(res.msg)
+                }
+            })
+            .done();
     }
     async removeToken(){
             try{
